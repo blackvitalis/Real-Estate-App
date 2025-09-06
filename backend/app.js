94cb88d42ed649f76089cf52d2c83f410e80dc27
blackvitalis <blackvitalis1224@gmail.com>
2025-09-06 13:25:19 +0100

    App.js Setup and database connection

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
 const express = require("express")
+const app = express()
+require("dotenv").config()
+const mongoose = require("mongoose")
+port = process.env.PORT || 3000
+
+mongoose.connect(process.env.DBURI).then(()=>{
+  console.log(`Database connected successfully`);
+}).catch((err)=>{
+  console.error("Failed to connect to mongoDB",err.message)
+})
+
+
+app.listen(port,()=>{
+console.log(`Server running on port ${port}❤️👌`);
+})
+
+
